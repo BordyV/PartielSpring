71d7dd33c5b172e3264facfa179a920451071e7d
BordyV <56049773+BordyV@users.noreply.github.com>
2022-02-18 16:54:33 +0100

    feat: eureka
    USER CASE:
    En tant qu’ingénieur système je souhaite avoir une interface de monitoring basé sur Eureka
    dans laquelle s’enregistre tout les services mis qui sont mis à disposition avant

--- show-weather/src/main/java/com/intense/bordy/showweather/ShowWeatherApplication.java
+++ show-weather/src/main/java/com/intense/bordy/showweather/ShowWeatherApplication.java
@@ -3,9 +3,11 @@ package com.intense.bordy.showweather;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EnableHystrixDashboard
 @EnableCircuitBreaker
 public class ShowWeatherApplication {
